eba3d3ac8d11072adf208a10d7a94f851bc65122
hritiksinghbms <71306174+hritiksinghbms@users.noreply.github.com>
2022-01-13 10:14:39 +0530

    Create lab6.sh

---
+++ LAB_6/lab6.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "Enter basic salary : \c"
+read pb
+echo "Enter da (in %) : \c"
+read da
+da=$((pb * da * 1/100))
+echo "Enter hra (in %) : \c"
+read hra
+hra=$((pb * hra * 1/100))
+echo "Enter pf (in %) : \c"
+read pf
+pf=$((pb * pf * 1/100))
+echo "Enter ta (in %) : \c"
+read ta
+ta=$((pb * ta * 1/100))
+gs=`expr $pb + $da + $hra + $ta - $pf`
+echo "Gross salary of employee is $gs"
+© 2022 GitHub, Inc.
+Terms
+Priva
